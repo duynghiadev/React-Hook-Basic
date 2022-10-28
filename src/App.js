425177159a3899f774ed5e3b5abe7ec6672d6e16
duynghia-123 <duynghia22302@gmail.com>
2022-10-29 04:50:21 +0700

    dynamic data in templates

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
+  let name = "Duy Nghia";
+  let number = 2022;
+  let obj = { name: "Nghia", chanel: "DuyNghia" };
+
   return (
     <div className="App">
+      {console.log("Object: ", obj)}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <h1>Hello World with React and Duy Nghia</h1>
+      <h1>
+        Hello World with React and {name} in {number}
+      </h1>
+      <p>{JSON.stringify(obj)}</p>
     </div>
   );
 };
