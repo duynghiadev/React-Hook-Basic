4bc0ad13dc54ef0c8767917790bf0719fb6993fa
duynghia-123 <duynghia22302@gmail.com>
2022-10-29 10:42:17 +0700

    add event to react

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 import Nav from "./views/Nav";
 
 const App = () => {
+  const handleEventClick = (event) => {
+    console.log(">>> Click me", event.target.value);
+  };
+
   return (
     <div className="App">
       <Nav />
@@ -10,6 +14,21 @@ const App = () => {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <h1>Hello World with ReactJs</h1>
+      <input
+        type="text"
+        value="basil"
+        onClick={(event) => {
+          handleEventClick(event);
+        }}
+      />
+      <button
+        type="button"
+        onClick={(event) => {
+          handleEventClick(event);
+        }}
+      >
+        Click me
+      </button>
     </div>
   );
 };
