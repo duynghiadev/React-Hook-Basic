61d86c4803fa4a744bc6ae6fc130ca2912a4cb1f
duynghia-123 <duynghia22302@gmail.com>
2022-10-29 08:38:07 +0700

    Multiple Components in React Hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./views/Nav";
 
 const App = () => {
-  let name = "Duy Nghia";
-  let number = 2022;
-  let obj = { name: "Nghia", chanel: "DuyNghia" };
-
   return (
     <div className="App">
-      {console.log("Object: ", obj)}
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <h1>
-        Hello World with React and {name} in {number}
-      </h1>
-      <p>{JSON.stringify(obj)}</p>
+      <h1>Hello World with ReactJs</h1>
     </div>
   );
 };
