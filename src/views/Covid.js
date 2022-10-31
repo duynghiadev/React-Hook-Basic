5d1cd3cfc11867508489f5d8d18d6cea9663793a
duynghia-123 <duynghia22302@gmail.com>
2022-10-31 07:48:59 +0700

    making custom hook

--- src/views/Covid.js
+++ src/views/Covid.js
@@ -1,33 +1,14 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
-import moment from "moment";
+import useFetch from "../customize/fetch";
 
 const Covid = () => {
-  const [dataCovid, setDataCovid] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(async () => {
-    try {
-      let res = await axios.get(
-        "https://api.covid19api.com/country/vietnam?from=2020-09-01T00:00:00Z&to=2020-11-01T00:00:00Z"
-      );
-      let data = res && res.data ? res.data : [];
-      if (data && data.length > 0) {
-        data.map((item) => {
-          item.Date = moment(item.Date).format("DD/MM/YYYY");
-          return item;
-        });
-        data = data.reverse();
-      }
-      setDataCovid(data);
-      setIsLoading(false);
-      setIsError(false);
-    } catch (error) {
-      setIsError(true);
-      setIsLoading(false);
-    }
-  }, []);
+  const {
+    data: dataCovid,
+    isLoading,
+    isError,
+  } = useFetch(
+    "https://api.covid19api.com/country/vietnam?from=2020-09-01T00:00:00Z&to=2020-11-01T00:00:00Z"
+  );
 
   return (
     <>
