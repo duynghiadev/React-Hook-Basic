fd030b67c1f3fa683a33b571f10c0649af9d7afd
duynghia-123 <duynghia22302@gmail.com>
2022-10-31 15:47:33 +0700

    hook cleanup

--- src/customize/fetch.js
+++ src/customize/fetch.js
@@ -6,11 +6,16 @@ const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
+
   useEffect(() => {
-    try {
-      async function fetchData() {
-        let res = await axios.get(url);
-        let data = res && res.data ? res.data : [];
+    const ourRequest = axios.CancelToken.source(); // <-- 1st step
+
+    async function fetchData() {
+      try {
+        let res = await axios.get(url, {
+          cancelToken: ourRequest.token, // <-- 2nd step
+        });
+        let data = res && res.data ? res.data : []; // true, false
         if (data && data.length > 0) {
           data.map((item) => {
             item.Date = moment(item.Date).format("DD/MM/YYYY");
@@ -21,13 +26,23 @@ const useFetch = (url) => {
         setData(data);
         setIsLoading(false);
         setIsError(false);
+      } catch (err) {
+        if (axios.isCancel(err)) {
+          console.log("Request canceled: ", err.message);
+        } else {
+          setIsError(true);
+          setIsLoading(false);
+        }
       }
-
-      fetchData();
-    } catch (e) {
-      setIsError(true);
-      setIsLoading(false);
     }
+
+    setTimeout(() => {
+      fetchData();
+    }, 3000);
+
+    return () => {
+      ourRequest.cancel("Operation canceled by the user"); // <-- 3rd step
+    };
   }, [url]);
 
   return {
